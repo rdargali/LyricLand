60878ee9d4850ed4b3d7eacc589c375ee3562420
Rawand Dargali <rdargali@yahoo.com>
2020-03-31 09:09:23 -0500

    context added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Index from "./components/Index";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-    </div>
+    <BrowserRouter>
+      <React.Fragment>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="" component={Index} />
+          </Switch>
+        </div>
+      </React.Fragment>
+    </BrowserRouter>
   );
 }
 
---
+++ src/context.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+const Context = React.createContext();
+
+export class Provider extends Component {
+  state = {
+    track_list: [
+        { track: { track_name: "abc" } }
+        { track: { track_name: "123" } }
+        { track: { track_name: "efg" } }
+        { track: { track_name: "456" } }
+    ],
+    heading: "today's top tracks"
+  };
+  render() {
+    return <Context.Provider value={this.state}>{this.props.children}</Context.Provider>;
+  }
+}
+
+export const Consumer = Context.Consumer;
