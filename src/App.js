dccbf9f279e870716c08db4bb0968f4c9024cf07
Rawand Dargali <rdargali@yahoo.com>
2020-03-31 17:00:01 -0500

    context global state working

--- src/App.js
+++ src/App.js
@@ -2,20 +2,25 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Index from "./components/Index";
+
+import { Provider } from "./context";
+
 import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
-      <React.Fragment>
-        <Navbar />
-        <div className="container">
-          <Switch>
-            <Route exact path="" component={Index} />
-          </Switch>
-        </div>
-      </React.Fragment>
-    </BrowserRouter>
+    <Provider>
+      <BrowserRouter>
+        <React.Fragment>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="" component={Index} />
+            </Switch>
+          </div>
+        </React.Fragment>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
--- src/context.js
+++ src/context.js
@@ -5,15 +5,19 @@ const Context = React.createContext();
 export class Provider extends Component {
   state = {
     track_list: [
-        { track: { track_name: "abc" } }
-        { track: { track_name: "123" } }
-        { track: { track_name: "efg" } }
-        { track: { track_name: "456" } }
+      { track: { track_name: "abc" } },
+      { track: { track_name: "123" } },
+      { track: { track_name: "efg" } },
+      { track: { track_name: "456" } }
     ],
     heading: "today's top tracks"
   };
   render() {
-    return <Context.Provider value={this.state}>{this.props.children}</Context.Provider>;
+    return (
+      <Context.Provider value={this.state}>
+        {this.props.children}
+      </Context.Provider>
+    );
   }
 }
 
