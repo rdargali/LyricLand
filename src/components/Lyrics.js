00ec1dd8174b05387f1df39ca4d72ec00bb74d45
Rawand Dargali <rdargali@yahoo.com>
2020-04-02 15:57:08 -0500

    lyrics and info displaying in lyrics componenet

--- src/components/Lyrics.js
+++ src/components/Lyrics.js
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
 import axios from "axios";
+import Spinner from "./Spinner";
+
+import { Link } from "react-router-dom";
 
 class Lyrics extends Component {
   state = {
-    tracks: {},
+    track: {},
     lyrics: {}
   };
 
@@ -13,20 +16,63 @@ class Lyrics extends Component {
         `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${this.props.match.params.id}&apikey=${process.env.REACT_APP_API_KEY}`
       )
       .then(res => {
-        console.log(res.data);
+        // console.log(res.data);
         this.setState({
-          //   track_list: res.data.message.body.track_list
+          lyrics: res.data.message.body.lyrics
+        });
+
+        return axios.get(
+          `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.get?track_id=${this.props.match.params.id}&apikey=${process.env.REACT_APP_API_KEY}`
+        );
+      })
+      .then(res => {
+        this.setState({
+          track: res.data.message.body.track
         });
       })
       .catch(err => console.log(err));
   }
 
   render() {
-    return (
-      <div>
-        <h1>{this.props.match.params.id}</h1>
-      </div>
-    );
+    const { track, lyrics } = this.state;
+
+    if (
+      track === undefined ||
+      lyrics === undefined ||
+      Object.keys(track).length === 0 ||
+      Object.keys(lyrics).length === 0
+    ) {
+      return <Spinner />;
+    } else {
+      return (
+        <React.Fragment>
+          <Link to="/" className="btn btn-dark btn-sm mb-4">
+            Go Back
+          </Link>
+          <div className="card">
+            <h5 className="card-header">
+              {track.track_name} by{" "}
+              <span className="text-secondary">{track.artist_name}</span>
+            </h5>
+            <div className="card-body">
+              <p className="card-text">{lyrics.lyrics_body}</p>
+            </div>
+          </div>
+          <ul className="list-group mt-3">
+            <li className="list-group-item">
+              <strong>Album</strong>: {track.album}
+            </li>
+            <li className="list-group-item">
+              <strong>Genre</strong>:{" "}
+              {
+                track.primary_genres.music_genre_list[0].music_genre
+                  .music_genre_name
+              }
+            </li>
+          </ul>
+        </React.Fragment>
+      );
+    }
   }
 }
 
