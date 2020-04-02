8eab6433e563aae955c6a19b38848057c8a787bf
Rawand Dargali <rdargali@yahoo.com>
2020-04-01 23:48:07 -0500

    css touch up

--- src/components/Track.js
+++ src/components/Track.js
@@ -24,7 +24,7 @@ const Track = props => {
             className="btn btn-dark btn-block"
           >
             <i className="fas fa-chevron-right" />
-            View Lyrics
+            View Lyric
           </Link>
         </div>
       </div>
