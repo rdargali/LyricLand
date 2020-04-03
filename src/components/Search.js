0bd600be85add3d238728fd14b58dec3ede8795d
Rawand Dargali <rdargali@yahoo.com>
2020-04-03 02:02:53 -0500

    remove console logs

--- src/components/Search.js
+++ src/components/Search.js
@@ -7,6 +7,25 @@ class Search extends Component {
     trackTitle: ""
   };
 
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    axios
+      .get(
+        `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.search?q_track=${this.state.trackTitle}&page_size=10&page=1&s_track_rating=desc&apikey=${process.env.REACT_APP_API_KEY}`
+      )
+      .then(res => {
+        console.log(res.data);
+      })
+      .catch(err => console.log(err));
+  };
+
   render() {
     return (
       <Consumer>
@@ -19,6 +38,25 @@ class Search extends Component {
               <p className="lead text-center">
                 Get the lyrics for all your favorite songs
               </p>
+              <form>
+                <div className="form-group">
+                  <input
+                    className="form-control form-control-lg"
+                    type="text"
+                    placeholder="Song title"
+                    name="trackTitle"
+                    value={this.state.trackTitle}
+                    onChange={this.handleChange}
+                  />
+                </div>
+              </form>
+              <button
+                className="btn btn-primary btn-lg btn-block mb-5"
+                type="submit"
+                onClick={this.handleSubmit}
+              >
+                Get Lyrics
+              </button>
             </div>
           );
         }}
