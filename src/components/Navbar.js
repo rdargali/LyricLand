b1cf86c3b1636d2f3059f530aa1790bd6efcbb40
Rawand Dargali <rdargali@yahoo.com>
2020-04-03 02:43:16 -0500

    minor grammar tweak

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = () => {
   return (
     <nav className="navbar navbar-dark bg-dark mb-5">
-      <span className="navbar-brand mb-0 h1 mx-auto"> LyricLand</span>
+      <span className="navbar-brand mb-0 h1 mx-auto"> Lyric Land</span>
     </nav>
   );
 };
