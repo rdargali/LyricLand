aaa2e91912bf9d470f9e9ee149e2d04c7bebd5c6
Rawand Dargali <rdargali@yahoo.com>
2020-04-03 02:39:54 -0500

    search feature complete

--- src/components/Search.js
+++ src/components/Search.js
@@ -13,7 +13,7 @@ class Search extends Component {
     });
   };
 
-  handleSubmit = e => {
+  handleSubmit = (dispatch, e) => {
     e.preventDefault();
 
     axios
@@ -21,7 +21,14 @@ class Search extends Component {
         `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.search?q_track=${this.state.trackTitle}&page_size=10&page=1&s_track_rating=desc&apikey=${process.env.REACT_APP_API_KEY}`
       )
       .then(res => {
-        console.log(res.data);
+        dispatch({
+          type: "SEARCH_TRACKS",
+          payload: res.data.message.body.track_list
+        });
+
+        this.setState({
+          trackTitle: ""
+        });
       })
       .catch(err => console.log(err));
   };
@@ -30,6 +37,7 @@ class Search extends Component {
     return (
       <Consumer>
         {value => {
+          const { dispatch } = value;
           return (
             <div className="card card-body mb-4 p-4">
               <h1 className="display-4 text-center">
@@ -38,7 +46,7 @@ class Search extends Component {
               <p className="lead text-center">
                 Get the lyrics for all your favorite songs
               </p>
-              <form>
+              <form onSubmit={this.handleSubmit.bind(this, dispatch)}>
                 <div className="form-group">
                   <input
                     className="form-control form-control-lg"
@@ -49,14 +57,13 @@ class Search extends Component {
                     onChange={this.handleChange}
                   />
                 </div>
+                <button
+                  className="btn btn-primary btn-lg btn-block mb-5"
+                  type="submit"
+                >
+                  Get Lyrics
+                </button>
               </form>
-              <button
-                className="btn btn-primary btn-lg btn-block mb-5"
-                type="submit"
-                onClick={this.handleSubmit}
-              >
-                Get Lyrics
-              </button>
             </div>
           );
         }}
